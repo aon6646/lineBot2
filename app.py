59161aec53819070924e1e6c832dcd2b96cb3191
aon6646 <52860019+aon6646@users.noreply.github.com>
2019-08-16 14:15:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-     if (userText == 'สวัสดี') :
+    if (userText == 'สวัสดี') :
         sendText(user,'เออ ดีด้วย')
     elif (userText == 'ไปนะ') :
         sendText(user,'ไปตายไหนก็ไป')
